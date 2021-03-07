23ef3c88f7ad55876e5de18190e566b77bad0e1d
unknown <2808117bulbiash@mail.ru>
2021-03-07 23:27:20 +0300

    Initial commit

---
+++ src/test/java/Tests/StudentForm.java
@@ -0,0 +1,66 @@
+package Tests;
+
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class StudentForm {
+
+    @Test
+
+    void studentFormTest() {
+        //Открыть ToolsQA
+        open("https://demoqa.com/automation-practice-form");
+        //Заполнить поле First Name
+        $("#firstName").setValue("John");
+        //Заполнить поле Last Name
+        $("#lastName").setValue("Seena");
+        //Заполнить поле Email
+        $("#userEmail").setValue("JohnSeena@mail.ru");
+        //Отметить Gender
+        $(byText("Male")).click();
+        //Заполнить поле Mobile
+        $("#userNumber").setValue("1111111111");
+        //Выбрать дату
+        $("#dateOfBirthInput").click();
+        $$(".react-datepicker__day").find(Condition.exactText("10")).click();
+        //Заполнить Sebjects
+        $("#subjectsInput").setValue("C");
+        $("#react-select-2-option-1").click();
+        //Отметить хобби
+        $(byText("Sports")).click();
+        $(byText("Music")).click();
+        //Загрузить фото
+        $("#uploadPicture").uploadFromClasspath("img/John.jpg");
+        //Скролим
+        $("#submit").scrollTo();
+        //Заполнить поле Current address
+        $("#currentAddress").setValue("Cannon street. House 3. Apartment 45");
+        //Выбрать State и City
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Merrut")).click();
+        //Нажимаем Submit
+        $("#submit").click();
+
+        //Проверяем
+
+        $x("//td[text()='Student Name']").parent().shouldHave(text("John Seena"));
+        $x("//td[text()='Student Email']").parent().shouldHave(text("JohnSeena@mail.ru"));
+        $x("//td[text()='Gender']").parent().shouldHave(text("Male"));
+        $x("//td[text()='Mobile']").parent().shouldHave(text("1111111111"));
+        $x("//td[text()='Date of Birth']").parent().shouldHave(text("10"));
+        $x("//td[text()='Subjects']").parent().shouldHave(text("Chemistry"));
+        $x("//td[text()='Hobbies']").parent().shouldHave(text("Sports, Music"));
+        $x("//td[text()='Picture']").parent().shouldHave(text("John.jpg"));
+        $x("//td[text()='Address']").parent().shouldHave(text("Cannon street. House 3. Apartment 45"));
+        $x("//td[text()='State and City']").parent().shouldHave(text("Uttar Pradesh Merrut"));
+
+    }
+}
